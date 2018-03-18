94ad54854aa942d6135af3f86599cfd9b886b900
Diego Fabricio <difagume@gmail.com>
2018-03-17 22:34:38 -0500

    Separando el login del template

--- src/app/app.routest.ts
+++ src/app/app.routest.ts
@@ -1,5 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { PagesComponent } from './pages/pages.component';
+
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ProgressComponent } from './pages/progress/progress.component';
@@ -7,12 +9,18 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graficas1', component: Graficas1Component },
+            { path: '', pathMatch: 'full', redirectTo: '/dashboard' }
+        ]
+    },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: LoginComponent },
-    { path: 'progress', component: ProgressComponent },
-    { path: 'graficas1', component: Graficas1Component },
-    { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
     { path: '**', component: NopagefoundComponent }
 ];
 
