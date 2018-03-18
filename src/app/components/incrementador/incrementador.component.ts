ddfcd34b3d3e6fa4636cdd91b4c5d21cde835d5c
Diego Fabricio <difagume@gmail.com>
2018-03-18 14:08:30 -0500

    @ViewChild - Referencia a un elemento HTML

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,6 +6,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styles: []
 })
 export class IncrementadorComponent implements OnInit {
+
+  @ViewChild('txtProgress') txtProgreso: ElementRef;
 
   @Input('textoLeyenda') leyenda: string = 'Leyenda';
   @Input() progreso: number = 50;
@@ -17,6 +19,24 @@ export class IncrementadorComponent implements OnInit {
   ngOnInit() {
   }
 
+  cuandoCambia(nuevoValor: number) {
+    // let elementoHTML: any = document.getElementsByName('progreso')[0];
+    // console.log(this.txtProgreso);
+
+    if (nuevoValor >= 100) {
+      this.progreso = 100;
+    } else if (nuevoValor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
+
+    // elementoHTML.value = this.progreso;
+    this.txtProgreso.nativeElement.value = this.progreso;
+
+    this.cambioValor.emit(this.progreso);
+  }
+
   cambiarValor(valor: number) {
     this.progreso = this.progreso + valor;
     if (this.progreso >= 100) {
@@ -26,6 +46,9 @@ export class IncrementadorComponent implements OnInit {
     }
 
     this.cambioValor.emit(this.progreso);
+
+    // foco en el input
+    this.txtProgreso.nativeElement.focus();
   }
 
 }
