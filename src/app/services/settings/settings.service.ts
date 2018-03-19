4979441ec332151fbd548d1d1b0f48468e3d801b
Diego Fabricio <difagume@gmail.com>
2018-03-18 21:29:35 -0500

    Servicio para controlar el Sidebar - Menú lateral

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -13,20 +13,19 @@ export class SettingsService {
   }
 
   guardarAjustes() {
-    console.log('Guardado en el localStorage');
-
+    // console.log('Guardado en el localStorage');
     localStorage.setItem('ajustes', JSON.stringify(this.ajustes));
   }
 
   cargarAjustes() {
     if (localStorage.getItem('ajustes')) {
       this.ajustes = JSON.parse(localStorage.getItem('ajustes'));
-      console.log('Cargando de localStorage');
+      //console.log('Cargando de localStorage');
 
       this.aplicarTema(this.ajustes.tema);
 
     } else {
-      console.log('Usando valores por defecto');
+      //console.log('Usando valores por defecto');
       this.aplicarTema(this.ajustes.tema);
     }
   }
