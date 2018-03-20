9893d1935ec1106694e7cd0bd85096b747b26bc4
Diego Fabricio <difagume@gmail.com>
2018-03-19 21:29:22 -0500

    Componente RxJs

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -21,6 +21,7 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -31,7 +32,8 @@ import { PromesasComponent } from './promesas/promesas.component';
         IncrementadorComponent,
         GraficoDonaComponent,
         AccountSettingsComponent,
-        PromesasComponent
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
