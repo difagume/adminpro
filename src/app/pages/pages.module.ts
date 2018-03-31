7e9f12b5a154670966de2a10d25f2291cfc8547b
Diego Fabricio <difagume@gmail.com>
2018-03-31 16:47:59 -0500

    Crear el componente del perfil del usuario

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -19,12 +19,12 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 // Pipe module
 import { PipesModule } from '../pipes/pipes.module';
 
-// temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     declarations: [
@@ -36,7 +36,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         GraficoDonaComponent,
         AccountSettingsComponent,
         PromesasComponent,
-        RxjsComponent
+        RxjsComponent,
+        ProfileComponent
     ],
     exports: [
         DashboardComponent,
