1827f7845b30b1dc163bb0153970c35e54e3951e
Diego Fabricio <difagume@gmail.com>
2018-03-31 21:34:23 -0500

    Crear los elementos en el menú

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -6,13 +6,22 @@ export class SidebarService {
   menu: any = [
     {
       titulo: 'Principal',
-      icono: 'mdi mdi-gauge',
+      icono: 'mdi mdi-gauge', // material design
       submenu: [
         { titulo: 'Dashboard', url: '/dashboard' },
         { titulo: 'ProgressBar', url: '/progress' },
         { titulo: 'Gráficas', url: '/graficas1' },
         { titulo: 'Promesas', url: '/promesas' },
         { titulo: 'RxJs', url: '/rxjs' }
+      ]
+    },
+    {
+      titulo: 'Mantenimientos',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu: [
+        { titulo: 'Usuarios', url: '/usuarios' },
+        { titulo: 'Hospitales', url: '/hospitales' },
+        { titulo: 'Médicos', url: '/medicos' }
       ]
     }
   ];
