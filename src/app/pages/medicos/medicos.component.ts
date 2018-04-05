0e1df22f76bbb9723291a03bb54617a1e4b5c463
Diego Guzmán <dfguzman@ceaaces.gob.ec>
2018-04-05 15:10:55 -0500

    Componente de Médicos y servicios básicos

--- src/app/pages/medicos/medicos.component.ts
+++ src/app/pages/medicos/medicos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MedicoService } from '../../services/service.index';
+import { Medico } from '../../models/medico.model';
+
+declare let swal: any;
 
 @Component({
   selector: 'app-medicos',
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  medicos: Medico[] = [];
+  desde: number = 0;
+  totalRegistros: number = 0;
+
+  constructor(
+    public _medicoService: MedicoService,
+  ) { }
 
   ngOnInit() {
+    // Cargo los médicos
+    this.cargarMedicos();
   }
+
+  cargarMedicos() {
+
+    this._medicoService.cargarMedicos(this.desde)
+      .subscribe((resp: any) => {
+
+        // console.log(resp);
+        this.totalRegistros = resp.total;
+        this.medicos = resp.medicos;
+      });
+  }
+
+  borrarMedico(medico: Medico) {
+    swal({
+      title: '¿Está seguro?',
+      text: 'Está a punto de borrar el ' + medico.nombre,
+      icon: 'warning',
+      buttons: ['Cancelar', true],
+      dangerMode: true,
+    })
+      .then(borrar => {
+        // console.log(borrar);
+        if (borrar) {
+
+          this._medicoService.borrarMedico(medico._id)
+            .subscribe(borrado => {
+              console.log(borrado);
+              this.cargarMedicos();
+            });
+        }
+      });
+  }
+
+
+
+  // Paginación
+  cambiarDesde(valor: number) {
+    const desde = this.desde + valor;
+    console.log(desde);
+
+    if (desde >= this.totalRegistros) {
+      return;
+    }
+
+    if (desde < 0) {
+      return;
+    }
+
+    this.desde += valor;
+    this.cargarMedicos();
+
+  }
+
+  /* mostrarModal(id: string) {
+    this._modalUploadService.mostrarModal('hospitales', id);
+  } */
 
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -1,8 +1,62 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioService } from '../usuario/usuario.service';
+import { URL_SERVICIOS } from '../../config/config';
+import { Medico } from '../../models/medico.model';
+
+declare let swal: any;
 
 @Injectable()
 export class MedicoService {
 
-  constructor() { }
+  constructor(
+    public _usuarioService: UsuarioService,
+    public http: HttpClient
+  ) { }
+
+  cargarMedicos(desde: number = 0) {
+    const url = URL_SERVICIOS + '/medico?desde=' + desde;
+    return this.http.get(url);
+  }
+
+  borrarMedico(id: string) {
+    const url = URL_SERVICIOS + '/medico/' + id + '?token=' + this._usuarioService.token;
+    return this.http.delete(url)
+      .map(resp => {
+        swal('Medico borrado', 'El médico ha sido eliminado correctamente', 'success');
+        return true;
+      });
+  }
+
+  crearMedico(nombre: string) {
+
+    const url = URL_SERVICIOS + '/medico/?token=' + this._usuarioService.token;
+
+    return this.http.post(url, { nombre })
+      .map((resp: any) => {
+        console.log(resp);
+
+        swal('Medico creado', nombre, 'success');
+        return resp.medico;
+      });
+  }
+
+  buscarMedicoes(termino: string) {
+    const url = URL_SERVICIOS + '/busqueda/coleccion/medicos/' + termino;
+    return this.http.get(url)
+      .map((resp: any) => resp.medicoes);
+  }
+
+  actualizarMedico(medico: Medico) {
+    const url = URL_SERVICIOS + '/medico/' + medico._id + '?token=' + this._usuarioService.token;
+
+    return this.http.put(url, medico)
+      .map((resp: any) => {
+
+        swal('Médico actualizado', medico.nombre, 'success');
+
+        return true;
+      });
+  }
 
 }
