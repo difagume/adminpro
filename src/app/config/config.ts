340961981b647b04ea6cc34b1b645377f0ef6b23
Diego Fabricio <difagume@gmail.com>
2018-04-18 22:45:17 -0500

    angular-progressive-web-app, firebase site

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-// export const URL_SERVICIOS = 'https://ancient-wildwood-10687.herokuapp.com';
-export const URL_SERVICIOS = 'http://localhost:5000';
+export const URL_SERVICIOS = 'https://ancient-wildwood-10687.herokuapp.com';
+// export const URL_SERVICIOS = 'http://localhost:5000';
